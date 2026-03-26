fe7b3644c2f0c363f1637b0cb4e90da928806f32
helpfulcraft <a1634358912@outlook.com>
2026-03-27 00:17:46 +0800

    chore: reconcile reddit workflows and remove heybox/bilibili legacy from root

--- project-nexus/app/api/bilibili-feeds/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from 'next/server';
-
-// Bilibili scanning has been completely removed as per user request.
-export async function GET() {
-    return NextResponse.json([]);
-}
